ed6d2ebe9671809bb0ac9fc73df59578d2cbb621
Nicolás Morales <nichromatic@users.noreply.github.com>
2019-01-26 19:44:14 +0100

    AudioManager general

---
+++ Welcome Home/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    List<Sound> musicTracks = new List<Sound>();
+    List<Sound> soundEffects = new List<Sound>();
+    AudioSource musicChannel = new AudioSource();
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        for (int i = 0; i < soundEffects.Count; i++)
+        {
+            AudioSource soundSrc = new AudioSource();
+            soundEffects[i].source = soundSrc;
+        }
+    }
+
+    public void playSound(string name)
+    {
+        AudioSource audiosrc = soundEffects.Find(s => s.name == name).source;
+        if (audiosrc.isPlaying)
+        {
+            audiosrc.Stop();
+        }
+        audiosrc.Play();
+    }
+
+    public void playMusic(string name)
+    {
+        if (musicChannel.isPlaying)
+        {
+            musicChannel.Stop();
+        }
+        musicChannel.clip = musicTracks.Find(m => m.name == name).clip;
+        musicChannel.volume = musicTracks.Find(m => m.name == name).volume;
+        musicChannel.pitch = musicTracks.Find(m => m.name == name).pitch;
+        musicChannel.Play();
+    }
+}
---
+++ Welcome Home/Assets/Scripts/Sound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.Audio;
+
+public class Sound
+{
+    public float volume;
+    public float pitch;
+    public AudioClip clip;
+    public AudioSource source;
+    public string name;
+
+    public Sound(string name = "clip", float volume = 1.0f, float pitch = 1.0f, AudioClip clip = null)
+    {
+        this.name = name;
+        this.volume = volume;
+        this.pitch = pitch;
+        this.clip = clip;
+    }
+}
