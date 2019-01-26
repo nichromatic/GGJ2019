44468ccf222de0e060d6841b349495e2ed3cdd02
Nicolás Morales <nichromatic@users.noreply.github.com>
2019-01-26 11:39:23 +0100

    Nivel 2 creado
    Creado nivl 2 faltan las rampas de esquina

---
+++ Welcome Home/Assets/ReturnToSpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToSpawn : MonoBehaviour
+{
+    Vector3 spawnPoint;
+
+    private void Start()
+    {
+        spawnPoint = GameObject.FindGameObjectWithTag("Player").transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.position = spawnPoint;
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
